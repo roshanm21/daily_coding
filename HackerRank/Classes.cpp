fd1f1a6522c6cb9e67db486333a4eb9263e86d7f
roshanm21 <roshworkspace01@gmail.com>
2026-08-03 23:52:34 +0530

    3 August Question Solved

--- HackerRank/Classes.cpp
+++ HackerRank/Classes.cpp
@@ -5,9 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
-    class Student
+class Student
     {
     private:
         int age;
@@ -55,4 +53,28 @@ int main()
             return ss.str();
         }
     };
-}
+
+
+///Hackerrank hidden code///
+    int main()
+    {
+        int age, standard;
+        string first_name, last_name;
+
+        cin >> age >> first_name >> last_name >> standard;
+
+        Student st;
+        st.set_age(age);
+        st.set_standard(standard);
+        st.set_first_name(first_name);
+        st.set_last_name(last_name);
+
+        cout << st.get_age() << "\n";
+        cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+        cout << st.get_standard() << "\n";
+        cout << "\n";
+        cout << st.to_string();
+
+        return 0;
+    }
+/// Hackerrank hidden code///
