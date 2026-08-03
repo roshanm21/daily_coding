3f8d818dbb36fecb68db2fbf78beec06df21c3f3
roshanm21 <roshworkspace01@gmail.com>
2026-08-03 23:49:21 +0530

    August 3 Question Solved

--- HackerRank/Classes.cpp
+++ HackerRank/Classes.cpp
@@ -1,4 +1,4 @@
-#include <cmath>
+#include <sstream>
 #include <cstdio>
 #include <vector>
 #include <iostream>
@@ -7,6 +7,52 @@ using namespace std;
 
 int main()
 {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    return 0;
+    class Student
+    {
+    private:
+        int age;
+        string firstname;
+        string lastname;
+        int standard;
+
+    public:
+        int get_age()
+        {
+            return age;
+        };
+        string get_first_name()
+        {
+            return firstname;
+        };
+        string get_last_name()
+        {
+            return lastname;
+        };
+        int get_standard()
+        {
+            return standard;
+        };
+        void set_age(int a)
+        {
+            age = a;
+        };
+        void set_first_name(string fn)
+        {
+            firstname = fn;
+        };
+        void set_last_name(string ln)
+        {
+            lastname = ln;
+        };
+        void set_standard(int std)
+        {
+            standard = std;
+        };
+        string to_string()
+        {
+            stringstream ss;
+            ss << age << "," << firstname << "," << lastname << "," << standard;
+            return ss.str();
+        }
+    };
 }
