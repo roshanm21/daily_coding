07ff3d4b83fdcf5b126a1403582d38bd74936d1f
roshanm21 <roshworkspace01@gmail.com>
2026-08-07 20:37:17 +0530

    7 August Question Solved

--- 100-Day Coding Sprint/Day 1/exchangingGifts.cpp
+++ 100-Day Coding Sprint/Day 1/exchangingGifts.cpp
@@ -1,9 +1,32 @@
+// Question Link -> https://unstop.com/code/challenge-assessment/250196?moduleId=372
+
 #include <iostream>
 #include <vector>
+using namespace std;
 
 void find_youngest_member(int n, int m, std::vector<std::pair<int, int>> &gifts)
 {
-    // Write your logic here.
+    // Write your logic here
+    vector<int> score(n + 1, 0); // array(size,default_value)
+    for (const auto &a : gifts)
+    {
+        int giver = a.first;
+        int reciv = a.second;
+
+        score[giver]--;
+        score[reciv]++;
+    }
+
+    int youngest = -1;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (score[i] == n - 1)
+        {
+            youngest = i;
+            break;
+        }
+    }
+    cout << youngest;
 }
 
 int main()
