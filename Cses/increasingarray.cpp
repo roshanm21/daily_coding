2995bfd4e4232c42404f20d179fe33939150c7dc
roshanm21 <roshworkspace01@gmail.com>
2026-08-09 23:00:50 +0530

    9 August Question Unsolved

---
+++ Cses/increasingarray.cpp
@@ -0,0 +1,24 @@
+// You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+// On each move, you may increase the value of any element by one. What is the minimum number of moves required?
+
+        // Input:
+        // The first input line contains an integer n: the size of the array.
+        // Then, the second line contains n integers x_1,x_2,.....,x_n: the contents of the array.
+
+        // Output:
+        // Print the minimum number of moves.
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int size{0};
+    cin>>size;
+    for(int j; j<size;j++){
+        int num{0};
+        cin>>num;
+    }
+    
+}
