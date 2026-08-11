276417758d98d6ee09921a9b21492e7a58e50067
roshanm21 <roshworkspace01@gmail.com>
2026-08-11 23:38:17 +0530

    9 August Question Solved O(n) Time complexity

--- 100-Day Coding Sprint/Day 1/distinctk.cpp
+++ 100-Day Coding Sprint/Day 1/distinctk.cpp
@@ -9,24 +9,26 @@ int main()
 {
     vector<string> mainstack = {};
     vector<string> dstack = {};
-    int num{}; string curr;
-    cin>>num;
-   
-    for(int i{0} ; i<num ; i++){
-        cin>>curr;
+    int num{};
+    string curr;
+    cin >> num;
+
+    for (int i{0}; i < num; i++)
+    {
+        cin >> curr;
         mainstack.push_back(curr);
     };
- 
+
     int q{};
-    cin>>q;
-    for(string j : mainstack){
-        cout<<j;   
-    auto it = find(dstack.begin(), dstack.end(), curr);
-    if (it == mainstack.end())
+    cin >> q;
+    for (string j : mainstack)
     {
-        dstack.push_back(curr);
-    }
+        auto it = find(dstack.begin(), dstack.end(), j);
+        if (it == dstack.end())
+        {
+            dstack.push_back(j);
+        }
     };
-    cout<<dstack[q];
+    cout << dstack[q + 1];
     return 0;
 }
