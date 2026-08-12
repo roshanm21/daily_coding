47c9193da19266e973e228761be144f9e40ff458
roshanm21 <roshworkspace01@gmail.com>
2026-08-12 22:00:07 +0530

    12 August Question Solved

--- 100-Day Coding Sprint/Day2/Pair-of-Subarrays.cpp
+++ 100-Day Coding Sprint/Day2/Pair-of-Subarrays.cpp
@@ -1,6 +1,21 @@
+/* 
+    QUESTION - 
+
+    You are given an array A of size N.
+    Your task is to count the number of unique pairs of non -
+    overlapping subarrays such that both subarrays have the same sum.
+    Each subarray is defined by a pair of indices[L, R](1 - based indexing),
+    where 1 ≤ L ≤ R ≤ N.
+    Two subarrays must not overlap,
+    meaning they should not share any common index.
+
+    Also,
+    the pair([ L1, R1 ], [ L2, R2 ]) is considered the same as([ L2, R2 ], [ L1, R1 ]), so count each valid pair only once.
+
+*/
+
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int calculate_pairs(int n, vector<int> &arr)
@@ -13,6 +28,12 @@ int calculate_pairs(int n, vector<int> &arr)
      * Returns:
      *     int: The required number of pairs based on the problem statement
      */
+
+    int pairs{};
+    for(int j : arr){
+        
+    }
+
 
     return 0;
 }
