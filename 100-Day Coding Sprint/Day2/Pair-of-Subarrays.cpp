85ae7292d2da8e6ce3888aff4d75dd376bb2b037
roshanm21 <roshworkspace01@gmail.com>
2026-08-12 16:57:26 +0530

    12 August Question Unsolved

--- 100-Day Coding Sprint/Day2/Pair-of-Subarrays.cpp
+++ 100-Day Coding Sprint/Day2/Pair-of-Subarrays.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calculate_pairs(int n, vector<int> &arr)
+{
+    /**
+     * Write your logic here.
+     * Parameters:
+     *     n (int): The size of the array
+     *     arr (vector<int>): Vector of integers representing the elements of the array
+     * Returns:
+     *     int: The required number of pairs based on the problem statement
+     */
+
+    return 0;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int result = calculate_pairs(n, arr);
+    cout << result << endl;
+    return 0;
+}
