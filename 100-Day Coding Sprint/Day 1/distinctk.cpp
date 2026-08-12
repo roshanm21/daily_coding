01a8fc9355298388316085e899a60a895dbdc870
roshanm21 <roshworkspace01@gmail.com>
2026-08-12 13:35:35 +0530

    11 August Question Solved

--- 100-Day Coding Sprint/Day 1/distinctk.cpp
+++ 100-Day Coding Sprint/Day 1/distinctk.cpp
@@ -29,6 +29,66 @@ int main()
             dstack.push_back(j);
         }
     };
-    cout << dstack[q + 1];
+    if (dstack.size() < q)
+    {
+        cout << -1 << "\n";
+    }
+    else
+    {
+        cout << dstack[q + 1];
+    }
+    
+    return 0;
+}
+
+//=======>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int num;
+    if (!(cin >> num))
+        return 0;
+
+    vector<string> mainstack(num);
+    unordered_map<string, int> freq; //Holds key,value pair
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> mainstack[i];
+        freq[mainstack[i]]++;
+    }
+
+    int q;
+    cin >> q;
+
+    vector<string> distinct_strings;
+    for (const string &s : mainstack)
+    {
+        if (freq[s] == 1)
+        {
+            distinct_strings.push_back(s);
+        }
+    }
+
+    if (q <= 0 || q > static_cast<int>(distinct_strings.size())) //static_cast<int>123.32 is like (int)123.32
+    {
+        cout << -1 << "\n";
+    }
+    else
+    {
+        cout << distinct_strings[q - 1] << "\n";
+    }
+
     return 0;
 }
