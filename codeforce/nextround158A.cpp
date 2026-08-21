ca71087c6658356626de6f9f30bf7265df3c1173
roshanm21 <roshworkspace01@gmail.com>
2026-08-21 23:59:48 +0530

    21 August Question Solved

--- codeforce/nextround158A.cpp
+++ codeforce/nextround158A.cpp
@@ -13,10 +13,15 @@ int main()
         cin>>num;
         nsort.push_back(num);
     }
-    if(nsort[p+1] >= nsort[p+2] && nsort[p+1]*nsort[p+2] != 0){
 
+    if(nsort[p+1] >= nsort[p+2] && nsort[p+1]*nsort[p+2] != 0){
+        if(nsort[p] == nsort[ap-1]){
+            cout << ap + 1;
+        }
+        else
+        {
             cout<<(p+1);
-        
+        }
     }else{
         cout<<0;
     }
