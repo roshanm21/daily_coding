fe681e138306fd350e166461ad3014b73335da1f
roshanm21 <roshworkspace01@gmail.com>
2026-08-21 23:48:58 +0530

    21 August Question (2/3) Test case passed

--- codeforce/nextround158A.cpp
+++ codeforce/nextround158A.cpp
@@ -4,4 +4,20 @@ using namespace std;
 
 int main()
 {
+    int ap{};
+    int p{};
+    cin>>ap>>p;
+    vector<int> nsort = {};
+    for(int j{0} ;j<ap;j++){
+        int num{0};
+        cin>>num;
+        nsort.push_back(num);
+    }
+    if(nsort[p+1] >= nsort[p+2] && nsort[p+1]*nsort[p+2] != 0){
+
+            cout<<(p+1);
+        
+    }else{
+        cout<<0;
+    }
 }
