bce5f283e0e023d4bcf9f4923acabdeea3e8e4c4
roshanm21 <roshworkspace01@gmail.com>
2026-08-23 18:57:39 +0530

    23 August Question Unsolved

---
+++ codeforce/boyorgirl236A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cin>>name;
+    vector<char> dist = {};
+    
+    for(char k : name){
+        auto val = find(dist.begin() , dist.end() ,k);
+        if(val != dist.end()){
+        }else{
+            dist.push_back(k);
+        }
+        cout << dist.size();
+    }
+    if((dist.size()%2)==0){
+        cout<<"CHAT WITH HER!";
+    }else {
+        cout<< "IGNORE HIM !";
+    }
+}
