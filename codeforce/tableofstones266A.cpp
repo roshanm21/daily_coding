d42bf712bff065b8b22d2f48487db35fe2a0aec9
roshanm21 <roshworkspace01@gmail.com>
2026-08-24 23:09:39 +0530

    24 August Question Unsolved (progress)

--- codeforce/tableofstones266A.cpp
+++ codeforce/tableofstones266A.cpp
@@ -5,6 +5,20 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main(){
+int stones{};
+vector<char> seq = {};
+char color;
+int count{};
+cin>>stones;
+for(int k{0} ; stones>k ;k++){
+    cin>>color;
+    seq.push_back(color);
+    if(seq.size() > 1){
+        if(seq[k-1] == seq[k]){
+            count++;
+        }
+    }
+}
+cout<<count;
 }
