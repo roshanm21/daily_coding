73be552ca928bdacc64b2debd7a54581264bd2d1
roshanm21 <roshworkspace01@gmail.com>
2026-08-25 23:57:44 +0530

    25 August Question Half Solved

--- codeforce/Petya-and-Strings112A.cpp
+++ codeforce/Petya-and-Strings112A.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
     // 'a' + 32 = 'A'
-    
+    string first;
+    string second;
+    cin>> first >> second;
+    for(char k : second){
+        if((int)k > 91){
+            
+        }
+    }
 }
