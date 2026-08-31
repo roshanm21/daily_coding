70d597da1fb2879c82d423dc351b8c1420ba117a
roshanm21 <roshworkspace01@gmail.com>
2026-08-31 12:51:01 +0530

    31 August Question Unsolved (error)

---
+++ Leetcode/addtwonums.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{   
+    vector<int> l1 = {2,4,3};
+    vector<int> l2 = {5,6,4};
+    vector<int> r1 = {0};
+    int l1n {0};
+    int l2n {0};
+
+    for(int o : l2){
+        l2n = 10*l2n + o;
+    }
+    for(int k : l1){
+        l1n = 10*l1n + k;
+    }
+    int sum = l1n + l2n;
+    if(sum==0){
+        r1.push_back(0);
+    }
+    else{
+        while (sum > 0){
+        r1.push_back(sum % 10);
+        sum /= 10;
+    }}
+
+}
