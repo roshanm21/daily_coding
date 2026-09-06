1030afa2e0a3cbc7f59d31b242c30d64ba13868b
roshanm21 <roshworkspace01@gmail.com>
2026-09-06 22:52:23 +0530

    6 September *stuck

--- Leetcode/longeststring.cpp
+++ Leetcode/longeststring.cpp
@@ -7,18 +7,25 @@ int main(){
     int size = 0;
     vector<char> store = {};
     
-    for(char p : s){
-        if(s[size] == s[size+1]){
-            cout<<endl;
-        }else {
-            auto finf = find(store.begin(),store.end(), p);
-            if(finf == store.end()){
-                cout<<p;
-                store.push_back(p);
-            };
-        }
-        size ++;
+    // for(char p : s){
+    //     if(s[size] == s[size+1]){
+    //         store.clear();
+    //     }else {
+    //         auto finf = find(store.begin(),store.end(), p);
+    //         if(finf == store.end()){
+    //             store.push_back(p);
+    //             cout<<p;
+    //         };
+    //     }
+    //     size++;
         
+    // }
+    // cout<<size;
+    for(char p : s){
+        size++;
+        cout<<p <<' '<<size <<endl;
+        
+
     }
 
 }
