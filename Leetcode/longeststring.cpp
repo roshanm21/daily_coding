2fc930b7e0da69c0466837a19da572963103056c
roshanm21 <roshworkspace01@gmail.com>
2026-09-07 22:38:24 +0530

    Learning Linked-lists

--- Leetcode/longeststring.cpp
+++ Leetcode/longeststring.cpp
@@ -3,29 +3,6 @@
 using namespace std;
 
 int main(){
-    string s = "pwwkew";
-    int size = 0;
-    vector<char> store = {};
-    
-    // for(char p : s){
-    //     if(s[size] == s[size+1]){
-    //         store.clear();
-    //     }else {
-    //         auto finf = find(store.begin(),store.end(), p);
-    //         if(finf == store.end()){
-    //             store.push_back(p);
-    //             cout<<p;
-    //         };
-    //     }
-    //     size++;
-        
-    // }
-    // cout<<size;
-    for(char p : s){
-        size++;
-        cout<<p <<' '<<size <<endl;
-        
-
-    }
+// will be updated soon 
 
 }
